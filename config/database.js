f05bf60a0e14334a5bb36076db4cb2f3cf9baff8
kathrinaAU <kathrina96@gmail.com>
2020-07-01 15:02:32 +0100

    Configurando base de datos

--- config/database.js
+++ config/database.js
@@ -11,6 +11,6 @@ const name = "entullo";
 
 module.exports = {
     // database:`mongodb://${host}:${port}/${name}`,
-    database:"ds123662.mlab.com:23662/heroku_zp6jl2nt",
+    database:"mongodb://ds123662.mlab.com:23662/heroku_zp6jl2nt",
     secret: 'yoursecret'
 }
