38a394a4dff2c0c04970790379ac3ccacdc882f3
alu0100913293 <alu0100913293@ull.edu.es>
2020-07-02 04:04:46 +0100

    Campos requeridos

--- models/planification.js
+++ models/planification.js
@@ -17,7 +17,7 @@ let planificationSchema = new Schema({
     _id: { type: String, required: true },
     description: { type: String, required: true },
     dias: { type: Number, required: true },
-    menus: {type: Array, required: false},
+    menus: {type: Array, required: true},
     date: { type: Date, default: Date.now },
 });
 
